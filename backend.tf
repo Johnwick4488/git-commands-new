b394086665868d4a0b0b80aab9a57e37c526b9ad
EC2 Default User <ec2-user@ip-172-31-41-141.ec2.internal>
2024-05-15 06:08:14 +0000

    Revert "14"
    This reverts commit a6f9ab35c7f238e3800c407ccd8bc103e65730d8.

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-bucket000044"
+    bucket         = "my-new-bucket000044"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
